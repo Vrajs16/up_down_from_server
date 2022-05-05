b00c41bf7467fc84e2d27c857d7f09be0997dd90
Vrajs16 <vrajs16108@gmail.com>
2022-05-05 08:49:05 -0400

    Saved Uploaded file to temp storage

--- server/static/js/script.js
+++ server/static/js/script.js
@@ -7,9 +7,11 @@ form.addEventListener("click", () => {
   fileInput.click();
 });
 
-fileInput.onchange = ({ target }) => {
-  let file = target.files[0];
-  if (file) {
+fileInput.onchange = async ({ target }) => {
+  fileInput.disabled = true;
+  const formData = new FormData();
+  for (const file of target.files) {
+    formData.set("file", file);
     let fileName = file.name;
     if (fileName.length > 12) {
       fileName =
@@ -17,18 +19,22 @@ fileInput.onchange = ({ target }) => {
         "... " +
         fileName.substr(fileName.lastIndexOf("."));
     }
-    uploadFile(fileName, file.size);
+    await uploadFile(fileName, file.size, formData);
   }
+  fileInput.disabled = false;
 };
 
-function uploadFile(name, size) {
-  let data = new FormData(form);
-  var request = new XMLHttpRequest();
-  request.responseType = "json";
-  request.upload.addEventListener("progress", ({ loaded, total }) => {
-    console.log("progress", loaded, total);
-    let fileLoaded = Math.floor((loaded / total) * 100);
-    let progressHTML = `<li class="row">
+function uploadFile(name, size, data) {
+  return new Promise((resolve, reject) => {
+    var request = new XMLHttpRequest();
+    request.responseType = "json";
+    request.addEventListener("progress", ({ loaded, total }) => {
+      // console.log("Download progress", loaded, total);
+    });
+    request.upload.addEventListener("progress", ({ loaded, total }) => {
+      // console.log(`${name} Upload progress`, loaded, total);
+      let fileLoaded = Math.floor((loaded / total) * 100);
+      let progressHTML = `<li class="row">
                             <i class="fas fa-file-alt"></i>
                             <div class="content">
                               <div class="details">
@@ -40,32 +46,34 @@ function uploadFile(name, size) {
                               </div>
                             </div>
                         </li>`;
-    progressArea.innerHTML = progressHTML;
-  });
-
-  request.addEventListener("load", (e) => {
-    if (request.status == 200) {
-      progressArea.innerHTML = "";
-      let uploadedHTML = `<li class="row">
-                                <div class="content">
-                                  <i class="fas fa-file-alt"></i>
-                                  <div class="details">
-                                    <span class="name">${name} • Uploaded</span>
-                                    <span class="size">${
-                                      Math.floor(size / 1000) / 1000
-                                    } MB</span>
+      progressArea.innerHTML = progressHTML;
+      if (loaded == total) {
+        progressArea.innerHTML = "";
+        let uploadedHTML = `<li class="row">
+                                  <div class="content">
+                                    <i class="fas fa-file-alt"></i>
+                                    <div class="details">
+                                      <span class="name">${name} • Uploaded</span>
+                                      <span class="size">${
+                                        Math.floor(size / 1000) / 1000
+                                      } MB</span>
+                                    </div>
                                   </div>
-                                </div>
-                                <i class="fa fa-check"></i>
-                              </li>`;
-      uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
-      console.log(`${request.response.message}`, "success");
-    } else {
-      console.log(`${request.response.message}`, "error");
-    }
-  });
+                                  <i class="fa fa-check"></i>
+                                </li>`;
+        uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
+      }
+    });
+    request.addEventListener("load", () => {
+      if (request.status == 200) {
+        resolve(request.response);
+      } else {
+        reject(request.status);
+      }
+    });
 
-  // Open and send the request
-  request.open("post", "/");
-  request.send(data);
+    // Open and send the request
+    request.open("post", "/");
+    request.send(data);
+  });
 }
--- server/views.py
+++ server/views.py
@@ -7,20 +7,17 @@ import os
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
-        # if 'file' not in request.files:
-        #     print("ERROR: No file part")
-        #     return redirect(request.url)
-        print(request.form)
-        file=""
-        # file = request.form['file']
+        if 'file' not in request.files:
+            print("ERROR: No file part")
+            return redirect(request.url)
         print("File uploaded")
-        print(file)
-        # if file.filename == '':
-        #     flash('No selected file')
-        #     return redirect(request.url)
-        # if file:
-        # filename = secure_filename(file.filename)
-        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         res = make_response(jsonify({"message": "File uploaded successfully"}),200)
         return res
     return render_template('index.html')
